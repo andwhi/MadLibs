82189c7296e58e9dee1a00ffb81349dfcb175ef2
Andrew Whitten <andrew.whitten@icloud.com>
2018-01-12 13:36:12 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace MadLibs
+{
+    class MainClass
+    {
+        public static Romeo romeoLib = new Romeo();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("MadLibs");
+            Console.WriteLine();
+            Console.WriteLine("By Andrew Whitten");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            Console.Clear();
+
+            MainClass prompt = new MainClass();
+            string selection = "";
+
+            prompt.Display();
+            while (!selection.ToUpper().Equals("Q"))
+            {
+                Console.WriteLine("Choice: ");
+                selection = Console.ReadLine();
+                prompt.Menu(selection);
+            }
+
+        }
+
+         void Display()
+        {
+           
+                Console.WriteLine("Which MadLib do you want to play?");
+                Console.WriteLine();
+                Console.WriteLine("R for Romeo and Juliet");
+                Console.WriteLine("L for Lucy In The Sky With Diamonds.");
+                Console.WriteLine("Q to quit.");
+
+
+        }
+
+       public void Menu(string selection)
+        {
+            bool quit = false;
+
+            while(!quit)
+            {
+                switch (selection.ToUpper())
+                {
+                    case "R":
+                        RomeoDisplay();
+                        quit = true;
+                        break;
+
+                    case "L":
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        void RomeoDisplay()
+        {
+
+            romeoLib.AddWords();
+            Console.WriteLine();
+            Console.WriteLine("Press enter to see the MadLib.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Two " + romeoLib.romeoArray.GetValue(0,1) + ", both alike" +
+                              "in dignity, In fair " + romeoLib.romeoArray.GetValue(1, 1) + ", where" +
+                              "we lay our scene, From ancient " + romeoLib.romeoArray.GetValue(2, 1) + " break");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to go back to the menu.");
+            Console.ReadLine();
+            Console.Clear();
+            Display();
+        }
+
+
+
+
+
+
+       
+           
+
+
+
+
+
+
+       
+    }
+}
---
+++ Romeo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace MadLibs
+{
+    public class Romeo
+    {
+        public string[,] romeoArray = new string[11, 2]
+        {
+            {"Noun (Plural): ", "" }, {"Place: ", ""}, {"Noun: ", ""},
+            {"Noun (Plural): ", ""}, {"Noun: ", ""}, {"Adjective: ", ""},
+            {"Verb: ", ""}, {"Number: ", ""}, {"Adjective: ", ""},
+            {"Body Part: ", ""}, {"Verb", ""}
+        };
+
+
+
+        public void AddWords()
+        {
+
+            for (int i = 0; i < romeoArray.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < romeoArray.GetUpperBound(1); j++)
+                {
+                    string part = romeoArray[i, j];  
+
+                }
+
+                Console.WriteLine(romeoArray.GetValue(i, 0));
+                romeoArray.SetValue(Console.ReadLine(), i, 1);
+
+            }
+
+        }
+
+    }
+
+
+}
+
